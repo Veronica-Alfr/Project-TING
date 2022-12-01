bda575e83a37f1b39984b606b4900267480c036d
Veronica-Alfr <veagalves@gmail.com>
2022-12-01 16:08:49 -0300

    feat: created process method

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if instance.contains_file(path_file):
+        return
+    lines = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(data)
 
 
 def remove(instance):
