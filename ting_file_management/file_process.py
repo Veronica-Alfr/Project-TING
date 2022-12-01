3ec749b06bcfbf4952d7daf12b8fadab8b9900bf
Veronica-Alfr <veagalves@gmail.com>
2022-12-01 16:14:37 -0300

    feat: created remove method

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,12 @@
 from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
 
 
 def process(path_file, instance):
     if instance.contains_file(path_file):
         return
     lines = txt_importer(path_file)
+
     data = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(lines),
@@ -14,8 +16,12 @@ def process(path_file, instance):
     instance.enqueue(data)
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue):
+    if not len(instance):
+        return print("Não há elementos")
+
+    data = instance.dequeue()
+    print(f"Arquivo {data['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
